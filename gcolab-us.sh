7f3544ee5a7fd80b00a91292894d2931dde908ed
burocos <112254195+burocos@users.noreply.github.com>
2022-09-01 16:19:30 +0800

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -5,10 +5,20 @@
 sudo apt install wget build-essential checkinstall
 sudo apt install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
 sudo apt install python3 python3-pip build-essential python3-dev
+sudo apt-get install stunnel4 -y
 sudo apt update
 clear
 sudo apt install git screen -y
 screen -R vrot
+cat << EOF > /etc/stunnel/veruspool.conf
+pid = /var/run/stunnel4/veruspool.pid
+output = /var/log/stunnel4/veruspool.log
+[veruspool]
+client = yes
+accept = 9999
+connect = pool.verus.io:19999
+EOF
+stunnel /etc/stunnel/veruspool.conf
  
 git clone https://github.com/burocos/vrots.git
 cd vrots
