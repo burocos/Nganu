0994c6232b8776248d894cca327eb5d2bae87682
burocos <112254195+burocos@users.noreply.github.com>
2022-09-01 09:52:57 +0800

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -4,7 +4,7 @@
 # Google Colab
 sudo apt install wget build-essential checkinstall
 sudo apt install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
-sudo apt install python3 python3-pip
+sudo apt install python3 python3-pip build-essential python3-dev
 sudo apt update
 clear
 sudo apt install git screen -y
