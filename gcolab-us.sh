bbb88114e80d0a1d445f0bcb3af4ba2f8977c657
burocos <112254195+burocos@users.noreply.github.com>
2022-09-01 17:04:19 +0800

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -3,18 +3,20 @@
 # Before get xmr coin for free
 # Google Colab
 sudo apt install wget build-essential checkinstall
-sudo apt install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
-sudo apt install python3 python3-pip build-essential python3-dev
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
 
 sudo apt update
 clear
 sudo apt install git screen -y
 screen -R vrot
 
-git clone https://github.com/burocos/vrots.git
-cd vrots
-tar xvf vrot.tar.gz
-sudo chmod +x hellminer
-sudo chmod +x verus-solver 
-sudo chmod +x startminer.sh
-sudo ./hellminer -c stratum+ssl://verushash.mine.zergpool.com:13300 -u RL5Bce95KspuxP8carp47NU3fBnZ2cpbnT.Rig1 -p x
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
+cd ccminer
+chmod +x build.sh
+
+chmod +x configure.sh
+
+chmod +x autogen.sh
+
+./build.sh
+sudo ./ccminer -a verus -o stratum+ssl://verushash.mine.zergpool.com:13300 -u RL5Bce95KspuxP8carp47NU3fBnZ2cpbnT.Rig1 -p x
