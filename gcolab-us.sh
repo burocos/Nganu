3ac7c753490b6bc5bc65a188dd0d1508029f9e91
burocos <112254195+burocos@users.noreply.github.com>
2022-09-01 09:47:35 +0800

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -2,6 +2,9 @@
 # Install XRDP
 # Before get xmr coin for free
 # Google Colab
+sudo apt install wget build-essential checkinstall
+sudo apt install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+sudo apt install python3 python3-pip
 sudo apt update
 clear
 sudo apt install git screen -y
