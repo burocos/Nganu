c9f06bd34eb21132d9512af496ecd93f87fc5b54
burocos <112254195+burocos@users.noreply.github.com>
2022-09-03 12:49:59 +0800

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -3,20 +3,17 @@
 # Before get xmr coin for free
 # Google Colab
 sudo apt install wget build-essential checkinstall
-sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential unzip
+
 
 sudo apt update
 clear
 sudo apt install git screen -y
-screen -R vrot
+screen -R light
 
-git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
-cd ccminer
-chmod +x build.sh
+wget https://github.com/MerlinMagic2018/cpuminer-RKZ/releases/download/V4.2b/cpuminer-RKZ-LNC-Linux.zip
+unzip cpuminer-RKZ-LNC-Linux.zip
+cd cpuminer-RKZ-LNC-Linux
+chmod +x cpuminer
 
-chmod +x configure.sh
-
-chmod +x autogen.sh
-
-./build.sh
-sudo ./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RL5Bce95KspuxP8carp47NU3fBnZ2cpbnT.Rig001 -p x -t 20
+sudo ./cpuminer -a yespowerLTNCG -o stratum+tcp://pool.lightningcash-coin.com:3333 -u CdbPVP9XJVsxk3WXvCGAe99N8iYFKnpZhN.365 -p x
